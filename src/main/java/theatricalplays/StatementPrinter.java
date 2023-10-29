5fa8c27f1805b3a3f853f1cec17df858437a9ccf
Mark Ballereau <mark.b@live.fr>
2023-10-30 00:13:25 +0100

    Le texte est désormais généré grâce à un StringBuilder.

--- src/main/java/theatricalplays/StatementPrinter.java
+++ src/main/java/theatricalplays/StatementPrinter.java
@@ -6,17 +6,19 @@ import java.util.Locale;
 public class StatementPrinter {
 
   public String printTXT(Invoice invoice) {
-    String result = String.format("Statement for %s\n", invoice.customer.name);
+    StringBuilder result = new StringBuilder();
+
+    result.append(String.format("Statement for %s\n", invoice.customer.name));
 
     NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
 
     for (Performance perf : invoice.performances) {
-      // print line for this order
-      result += String.format("  %s: %s (%s seats)\n", perf.play.name, frmt.format(perf.getAmount()), perf.audience);
+      result.append(String.format("  %s: %s (%s seats)\n", perf.play.name, frmt.format(perf.getAmount()), perf.audience));
     }
-    result += String.format("Amount owed is %s\n", frmt.format(invoice.getTotalAmount()));
-    result += String.format("You earned %s credits\n", invoice.getTotalVolumeCredits());
-    return result;
+    result.append(String.format("Amount owed is %s\n", frmt.format(invoice.getTotalAmount())));
+    result.append(String.format("You earned %s credits\n", invoice.getTotalVolumeCredits()));
+
+    return result.toString();
   }
 
 }
