146398f751cced3da5707b2ca2d43ac121da12d1
Mark Ballereau <mark.b@live.fr>
2023-10-30 01:24:47 +0100

    Ajout du rendu HTML + tests.

--- src/main/java/theatricalplays/StatementPrinter.java
+++ src/main/java/theatricalplays/StatementPrinter.java
@@ -8,11 +8,10 @@ public class StatementPrinter {
   public String printTXT(Invoice invoice) {
     InvoiceData invoiceData = invoice.getData();
 
-    StringBuilder result = new StringBuilder();
-
-    result.append(String.format("Statement for %s\n", invoiceData.customerName));
-
     NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
+
+    StringBuilder result = new StringBuilder(String.format("Statement for %s\n", invoiceData.customerName));
+
     for (int i = 0; i < invoiceData.performances.length; i++) {
       result.append(String.format("  %s: %s (%s seats)\n", invoiceData.performances[i].playName, frmt.format(invoiceData.performances[i].amount), invoiceData.performances[i].audience));
     }
@@ -22,4 +21,22 @@ public class StatementPrinter {
     return result.toString();
   }
 
+  public String printHTML(Invoice invoice) {
+    InvoiceData invoiceData = invoice.getData();
+
+    NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
+
+    StringBuilder result = new StringBuilder(String.format("<h1>Statement for %s</h1>\n", invoiceData.customerName));
+
+    result.append("<table border='1'>\n");
+    result.append("  <tr><th>play</th><th>seats</th><th>price</th></tr>\n");
+    for (int i = 0; i < invoiceData.performances.length; i++) {
+      result.append(String.format("  <tr><td>%s</td><td>%s</td><td>%s</td></tr>\n", invoiceData.performances[i].playName, invoiceData.performances[i].audience, frmt.format(invoiceData.performances[i].amount)));
+    }
+    result.append("</table>\n");
+    result.append(String.format("<p>Amount owed is <em>%s</em></p>\n", invoiceData.totalAmount));
+    result.append(String.format("<p>You earned <em>%s</em> credits</p>\n", invoiceData.totalVolumeCredits));
+
+    return result.toString();
+  }
 }
--- src/test/java/theatricalplays/StatementPrinterTests.java
+++ src/test/java/theatricalplays/StatementPrinterTests.java
@@ -5,11 +5,12 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.approvaltests.Approvals.verify;
+import static org.approvaltests.Approvals.verifyHtml;
 
 public class StatementPrinterTests {
 
     @Test
-  void exampleStatement() {
+  void testTXTInvoice() {
     Customer BigCo = new Customer("BigCo");
 
     Play Hamlet = new Tragedy("Hamlet");
@@ -25,5 +26,24 @@ public class StatementPrinterTests {
     var result = statementPrinter.printTXT(invoice);
 
     verify(result);
+  }
+
+  @Test
+  void testHTMLInvoice() {
+    Customer BigCo = new Customer("BigCo");
+
+    Play Hamlet = new Tragedy("Hamlet");
+    Play AsYouLikeIt = new Comedy("As You Like It");
+    Play Othello = new Tragedy("Othello");
+
+    Invoice invoice = new Invoice(BigCo, List.of(
+      new Performance(Hamlet, 55),
+      new Performance(AsYouLikeIt, 35),
+      new Performance(Othello, 40)));
+
+    StatementPrinter statementPrinter = new StatementPrinter();
+    var result = statementPrinter.printHTML(invoice);
+
+    verifyHtml(result);
   }
 }
